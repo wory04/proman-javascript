839d06a83f82046be4ca88a83c6430fa75f13db4
wory04 <voroseszterveronika@gmail.com>
2019-09-18 15:25:12 +0200

    fix get_board route function

--- main.py
+++ main.py
@@ -20,17 +20,7 @@ def get_boards():
     """
     All the boards
     """
-    return data_handler.get_boards()
-
-
-@app.route("/get-cards/<int:board_id>")
-@json_response
-def get_cards_for_board(board_id: int):
-    """
-    All cards that belongs to a board
-    :param board_id: id of the parent board
-    """
-    return data_handler.get_cards_for_board(board_id)
+    return data_handler.get_boards_with_content()
 
 
 def main():

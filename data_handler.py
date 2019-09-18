994bfac1e075ba5ac099a8c385792f3a261a69b5
wory04 <voroseszterveronika@gmail.com>
2019-09-18 15:26:53 +0200

    implement get_boards_with_content function

--- data_handler.py
+++ data_handler.py
@@ -1,31 +1,10 @@
-import persistence
 import db_common
 
 
-def get_card_status(status_id):
-    """
-    Find the first status matching the given id
-    :param status_id:
-    :return: str
-    """
-    statuses = persistence.get_statuses()
-    return next((status['title'] for status in statuses if status['id'] == str(status_id)), 'Unknown')
-
-
-def get_boards():
-    """
-    Gather all boards
-    :return:
-    """
-    return persistence.get_boards(force=True)
-
-
-def get_cards_for_board(board_id):
-    persistence.clear_cache()
-    all_cards = persistence.get_cards()
-    matching_cards = []
-    for card in all_cards:
-        if card['board_id'] == str(board_id):
-            card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
-            matching_cards.append(card)
-    return matching_cards
+def get_boards_with_content():
+    boards = db_common.get_all_from_table('board')
+    for board in boards:
+        board['statuses'] = db_common.get_all_from_table_by_outer_table_id('status', 'board_id', str(board['id']))
+        for status in board['statuses']:
+            status['cards'] = db_common.get_all_from_table_by_outer_table_id('card', 'status_id', str(status['id']))
+    return boards

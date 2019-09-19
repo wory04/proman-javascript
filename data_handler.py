1cc6f124c167bd7d86791f0931b76a0aca8c420a
Agnes Kadlot <agnes.kadlot@gmail.com>
2019-09-19 10:09:51 +0200

    Create new_board function for sql

--- data_handler.py
+++ data_handler.py
@@ -8,3 +8,7 @@ def get_boards_with_content():
         for status in board['statuses']:
             status['cards'] = db_common.get_all_from_table_by_outer_table_id('card', 'status_id', str(status['id']))
     return boards
+
+
+def create_new_board():
+    return db_common.create_new_board()

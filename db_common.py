89853d080a021dcba54b2e69ea5758756f10e627
wory04 <voroseszterveronika@gmail.com>
2019-09-30 22:24:42 +0200

    implement save_new_registration, get_password_by_username, get_all_users

--- db_common.py
+++ db_common.py
@@ -67,3 +67,35 @@ def update_title_by_id(cursor, id_number, title, table):
 
     result = cursor.fetchone()
     return result
+
+
+@db_connection.connection_handler
+def save_new_registration(cursor, user_data):
+    insert_data = sql.SQL('''
+        INSERT INTO user_info (name, password) VALUES ({})
+        RETURNING *;
+        ''').format(
+        sql.SQL(', ').join(sql.Placeholder() * len(user_data)))
+
+    cursor.execute(insert_data, user_data)
+    new_user = cursor.fetchone()
+
+    return new_user
+
+
+@db_connection.connection_handler
+def get_password_by_username(cursor, username):
+    query_for_pwd = sql.SQL("SELECT password FROM user_info WHERE name = {}").format(sql.Placeholder())
+    cursor.execute(query_for_pwd, [username])
+    password = cursor.fetchone()
+
+    return password
+
+
+@db_connection.connection_handler
+def get_all_users(cursor):
+    query_for_users = sql.SQL("SELECT name FROM user_info")
+    cursor.execute(query_for_users)
+    user_names = cursor.fetchall()
+
+    return user_names

5c99c5230c4ca1b6e8930e7ba77390a431f24f2a
wory04 <voroseszterveronika@gmail.com>
2019-09-30 14:23:41 +0200

    add password hashing

--- data_handler.py
+++ data_handler.py
@@ -1,4 +1,5 @@
 import db_common
+import bcrypt
 
 
 def get_boards_with_content():
@@ -44,3 +45,14 @@ def rename_card(card_data):
     card_id = card_data['id']
     card_title = card_data['title']
     return db_common.update_title_by_id(card_id, card_title, 'card')
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
